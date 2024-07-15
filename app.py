cb884cc23090579399e8f79e2bb8596b2b64afbf
Joao Eduardo <joaoedu@gmail.com>
2024-07-15 17:21:35 -0300

    atualização tutorial

--- app.py
+++ app.py
@@ -1,4 +1,73 @@
 from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import letter, A4
 
-c = canvas.Canvas("hello-world.pdf")
+def hello(c):
+    from reportlab.lib.units import inch
+    # move the origin up and to the left
+    c.translate(inch,inch)
+    # define a large font
+    c.setFont("Helvetica", 14)
+    # choose some colors
+    c.setStrokeColorRGB(0.2,0.5,0.3)
+    c.setFillColorRGB(1,0,1)
+
+    # draw some lines
+    c.line(0,0,0,1.7*inch)
+    c.line(0,0,1*inch,0)
+    # draw a rectangle
+    c.rect(0.2*inch,0.2*inch,1*inch,1.5*inch, fill=1)
+    # make text go straight up
+    c.rotate(90)
+    # change color
+    c.setFillColorRGB(0,0,0.77)
+    # say hello (note after rotate the y coord needs to be negative!)
+    c.drawString(0.3*inch, -inch, "Hello World")
+
+def coords(canvas):
+    from reportlab.lib.units import inch
+    from reportlab.lib.colors import pink, black, red, blue, green
+    c = canvas
+    c.setStrokeColor(pink)
+    c.grid([inch, 2*inch, 3*inch, 4*inch], [0.5*inch, inch, 1.5*inch, 2*inch, 2.5*inch])
+    c.setStrokeColor(black)
+    c.setFont("Times-Roman", 20)
+    c.drawString(0,0, "(0,0) the Origin")
+    c.drawString(2.5*inch, inch, "(2.5,1) in inches")
+    c.drawString(4*inch, 2.5*inch, "(4, 2.5)")
+    c.setFillColor(red)
+    c.rect(0,2*inch,0.2*inch,0.3*inch, fill=1)
+    c.setFillColor(green)
+    c.circle(4.5*inch, 0.4*inch, 0.2*inch, fill=1)
+
+def translate(canvas):
+    from reportlab.lib.units import cm
+    canvas.translate(2.3*cm, 0.3*cm)
+    coords(canvas)
+
+def scale(canvas):
+    canvas.scale(0.75, 0.5)
+    coords(canvas)
+
+def scaletranslate(canvas):
+    from reportlab.lib.units import inch
+    canvas.setFont("Courier-BoldOblique", 12)
+    # save the state
+    canvas.saveState()
+    # scale then translate
+    canvas.scale(0.3, 0.5)
+    canvas.translate(2.4*inch, 1.5*inch)
+    canvas.drawString(0, 2.7*inch, "Scale then translate")
+    coords(canvas)
+    # forget the scale and translate...
+    canvas.restoreState()
+    # translate then scale
+    canvas.translate(2.4*inch, 1.5*inch)
+    canvas.scale(0.3, 0.5)
+    canvas.drawString(0, 2.7*inch, "Translate then scale")
+    coords(canvas)
+
+c = canvas.Canvas("hello.pdf", pagesize=A4)
+width, height = A4
+scaletranslate(c)
+c.showPage()
 c.save()
